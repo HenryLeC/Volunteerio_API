cf85c2ef081c25b785f8d38422eda005d8bcd0aa
Henry LeCompte <lecompteh18@gmail.com>
2020-07-26 12:13:42 -0400

    Removed email from DbSetup

--- DbSetup.py
+++ DbSetup.py
@@ -10,13 +10,13 @@ def makeDB():
     s = School("Highland Oaks Middle")
 
     u1 = User("U1", "12345", "User, User 1", "1111111", d,
-              s, "lecompteh18@gmail.com", student=True)
+              s, student=True)
     u2 = User("U2", "12345", "User, User 2", "2222222", d,
-              s, "2222222@volunteerio.us", admin=True)
+              s, admin=True)
     u3 = User("U3", "12345", "User, User 3", "3333333", d,
-              s, "3333333@volunteerio.us", community=True)
+              s, community=True)
     u4 = User("U4", "12345", "User, User 4", "4444444", None,
-              None, "4444444@volunteerio.us", webmaster=True)
+              None,  webmaster=True)
 
     db.session.add(d)
     db.session.add(u1)
