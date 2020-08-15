857e564b746e7cbb2cdaad344e8f3ef2f7a53085
Henry LeCompte <lecompteh18@gmail.com>
2020-08-15 17:53:49 -0400

    Added static_url_path to webapp Blueperint

--- API/__init__.py
+++ API/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 api = Blueprint('api', __name__)
-webapp = Blueprint('webapp', __name__)
+webapp = Blueprint('webapp', __name__, static_folder='static', static_url_path='/webapp/static')
 CORS(app, supports_credentials=True)
 
 # Set path for DB
