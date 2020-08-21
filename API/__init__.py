d7be811dec81da6bf5f00c32fb7b22523f392164
Henry LeCompte <32208783+HenryLeC@users.noreply.github.com>
2020-08-21 12:05:30 -0400

    Changed Secret Key/Database URI to be untracked (#2)
    * Make All Routes return a valid response webadmin
    
    * Changed Secret Key/Database URI to be untracked

--- API/__init__.py
+++ API/__init__.py
@@ -4,12 +4,14 @@ import os
 from PrefixMiddleware import PrefixMiddleware
 from flask_cors import CORS
 from flask_migrate import Migrate
+from API.Secrets import SecretKey, DatabaseURI
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-app.config['SECRET_KEY'] = "9yp0IPC62yB6AScoJ8APFOaYEO6vIDy_MvL7p3SRQ-Q"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://volunteerio:studio@postgres:5432/volunteerio?client_encoding=utf8'
+# Set Flask Config
+app.config['SECRET_KEY'] = SecretKey
+app.config['SQLALCHEMY_DATABASE_URI'] = DatabaseURI
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/api')
 
 db = SQLAlchemy(app)
